9c625241203ee9210a3c4e2f0f773a21d45c5481
SAS_OP <14040386+sasop3@users.noreply.github.com>
2025-06-25 09:04:34 +0300

    added placeholder icon for image

--- dtr_tf2_program/src/main/java/dtr/App.java
+++ dtr_tf2_program/src/main/java/dtr/App.java
@@ -2,17 +2,10 @@ package dtr;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Text;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
-
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.FileSystem;
 
 /**
  * JavaFX App
@@ -26,10 +19,11 @@ public class App extends Application {
         Parent root = loader.load();
         Scene scene = new Scene(root);
         stage.setScene(scene);
+        Image icon = new Image(getClass().getResourceAsStream("Star_of_my_own.png"));
+        stage.getIcons().add(icon);
         stage.setResizable(false);
         stage.show();
         Controller tf = loader.getController();
-
 
         tf.startup();
 
